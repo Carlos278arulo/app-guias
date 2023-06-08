03ec77f1d6685d8866af99ebc8746c29f6cb2124
Carlos278arulo <cl5343940@gmail.com>
2023-06-07 23:42:41 -0500

    nuevos cambios

--- src/controllers/archivocontroler.js
+++ src/controllers/archivocontroler.js
@@ -1,6 +1,16 @@
 const fs = require("fs");
 const Archivo = require("../models/archivos");
 const Guia = require("../models/fil");
+const nodemailer = require('nodemailer');
+
+// Configurar el transporter para el servicio de correo
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'carlosjoseariaslopez@gmail.com', // Tu dirección de correo electrónico
+    pass: 'mqtdukqzaewjzece', // Tu contraseña de correo electrónico
+  },
+});
 
 const subirArchivo = async (req, res) => {
   try {
@@ -21,6 +31,7 @@ const subirArchivo = async (req, res) => {
 
     // Eliminar el archivo temporal después de guardarlo en la base de datos
     fs.unlinkSync(archivo.path);
+
     req.flash("success_msg", "Archivo subido");
     res.redirect("/guias/home");
   } catch (error) {
@@ -38,6 +49,7 @@ const enviarFormulario = async (req, res) => {
       return res.status(400).send("No se ha proporcionado ningún archivo");
     }
 
+    // Guardar los datos en la base de datos
     const nuevaGuia = new Guia({
       nombre,
       carrera,
@@ -51,21 +63,65 @@ const enviarFormulario = async (req, res) => {
 
     await nuevaGuia.save();
 
+    // Enviar el correo de notificación
+    const mailOptions = {
+      from: 'carlosjoseariaslopez@gmail.com', // Tu dirección de correo electrónico
+      to: 'cl5343940@gmail.com', // Dirección de correo del destinatario
+      subject: 'Nuevo formulario enviado',
+      text: `Se ha recibido un nuevo formulario con los siguientes datos:
+        Nombre: ${nombre}
+        Carrera: ${carrera}
+        Asignatura: ${asignatura}
+        Grupo: ${grupo}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        // Manejar el error, si corresponde
+      } else {
+        console.log('Correo enviado: ' + info.response);
+        // Manejar la notificación de éxito, si corresponde
+      }
+    });
+
     // Eliminar el archivo temporal después de guardarlo en la base de datos
     fs.unlinkSync(archivo.path);
 
-    req.flash("success_msg", "Guía de laboratorio agregada exitosamente");
-    res.redirect("/guias/subirguia"); // Redirige a la página principal o a donde desees después de subir la guía
+    res.send('Formulario enviado correctamente');
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error al subir la guía");
+    res.status(500).send("Error al enviar el formulario");
   }
 };
-
-
 
 module.exports = {
   subirArchivo,
   enviarFormulario,
-  
+};
+
+const descargarArchivo = async (req, res) => {
+  try {
+    // Obtener el primer archivo encontrado en la base de datos
+    const archivo = await Archivo.findOne();
+
+    if (!archivo) {
+      return res.status(404).send("No se encontró ningún archivo en la base de datos");
+    }
+
+    // Establecer el tipo de contenido como PDF
+    res.setHeader("Content-Type", "application/pdf");
+
+    // Enviar el archivo como respuesta
+    res.send(archivo.archivo.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al descargar el archivo");
+  }
+};
+
+module.exports = {
+  subirArchivo,
+  enviarFormulario,
+  descargarArchivo,
 };
--- src/routes/archivo.js
+++ src/routes/archivo.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const archivoController = require("../controllers/archivocontroler");
-
-
-
+const nodemailer = require('nodemailer');
+const fs = require("fs");
+const Guia = require("../models/fil");
 
 // Configuración de Multer para el almacenamiento de archivos
 const storage = multer.diskStorage({
@@ -15,7 +15,6 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + "-" + file.originalname); // Nombre del archivo
   }
 });
-
 
 // Validación de archivos
 const fileFilter = (req, file, cb) => {
@@ -35,13 +34,75 @@ const upload = multer({
   }
 });
 
+// Configurar el transporter para el servicio de correo
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'carlosjoseariaslopez@gmail.com', // Tu dirección de correo electrónico
+    pass: 'mqtdukqzaewjzece', // Tu contraseña de correo electrónico
+  },
+});
+
 // Ruta para subir un archivo
 router.post("/subir", upload.single("archivo"), archivoController.subirArchivo);
-// Ruta para consultar los datos de archivos
-router.post("/enviar", upload.single("guia"), archivoController.enviarFormulario);
 
+// Ruta para enviar el formulario y enviar la notificación por correo electrónico
+router.post("/enviar", upload.single("guia"), async (req, res) => {
+  try {
+    // Obtener los datos del formulario
+    const nombre = req.body.nombre;
+    const carrera = req.body.carrera;
+    const asignatura = req.body.asignatura;
+    const grupo = req.body.grupo;
 
+    // Configurar los detalles del correo de notificación
+    const mailOptions = {
+      from: 'carlosjoseariaslopez@gmail.com', // Tu dirección de correo electrónico
+      to: 'cl5343940@gmail.com',              // Dirección de correo del destinatario
+      subject: 'Nuevo formulario enviado',
+      text: `Se ha recibido un nuevo formulario con los siguientes datos:
+        Nombre: ${nombre}
+        Carrera: ${carrera}
+        Asignatura: ${asignatura}
+        Grupo: ${grupo}`,
+    };
+
+    // Guardar los datos en la base de datos
+    const archivo = req.file;
+    const nuevaGuia = new Guia({
+      nombre,
+      carrera,
+      asignatura,
+      grupo,
+      guia: {
+        data: fs.readFileSync(archivo.path),
+        contentType: archivo.mimetype,
+      },
+    });
+
+    await nuevaGuia.save();
+
+    // Enviar el correo de notificación
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        // Manejar el error, si corresponde
+      } else {
+        console.log('Correo enviado: ' + info.response);
+        // Manejar la notificación de éxito, si corresponde
+      }
+    });
+
+    // Eliminar el archivo temporal después de guardarlo en la base de datos
+    fs.unlinkSync(archivo.path);
+
+    res.send('Formulario enviado correctamente');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al enviar el formulario");
+  }
+});
+// Ruta para descargar un archivo
+router.get("/descargar", archivoController.descargarArchivo);
 
 module.exports = router;
-
-
